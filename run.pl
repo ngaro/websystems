9d3a0e9da8838fb35910a5b7e2ec3b82cad7906a
Nikolas Garofil <nikolas.garofil@uantwerpen.be>
2021-09-14 23:25:57 +0200

    --secps to choose what to use with ttyd as init. init.c has not been adjusted yet

--- run.pl
+++ run.pl
@@ -6,8 +6,8 @@ use Getopt::Long;
 my $image="websystem"; my $container="websystem";
 my $tag="latest";
 my $hostname="websystem";
-my $firstpid="login";	#user can only get root rights when in sudo
-#my $firstpid="/bin/bash";	#user will have root rights
+my $secondprocess="login";	#user can only get root rights when in sudo
+#my $secondprocess="/bin/bash";	#user will have root rights
 my $interactive="";
 
 GetOptions(
@@ -15,7 +15,7 @@ GetOptions(
 	"container=s", => \ $container,
 	"tag=s", => \ $tag,
 	"hostname=s", => \ $hostname,
-	"firstpid=s", => \ $firstpid,
+	"secps=s", => \ $secondprocess,
 	"interactive", => \ $interactive,
 ) or die "Wrong arguments";
 my $how="--rm --init --hostname $hostname --name $container";
@@ -24,6 +24,6 @@ if($interactive ne "") {
 } else {
 	$how .= " -d";
 }
-system "docker run $how --hostname $hostname --name $container $image:$tag ttyd $firstpid";
+system "docker run $how --hostname $hostname --name $container $image:$tag init $secondprocess";
 my $ip = `docker inspect --format '{{ .NetworkSettings.IPAddress }}' $container`; chomp $ip;
 if($interactive eq "") { print "You can access the interface at http://$ip:7681\n"; }
